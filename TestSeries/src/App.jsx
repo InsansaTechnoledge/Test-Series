bb2d148ec25f13276753e13ff1d5667faf614d9b
Tanmay seth <tanmayseth@Tanmays-MacBook-Air.local>
2025-05-14 14:03:38 +0530

    added bgblue

--- TestSeries/src/App.jsx
+++ TestSeries/src/App.jsx
@@ -3,9 +3,7 @@ import React from 'react'
 const App = () => {
   return (
     <div>
-      <h1 class="text-3xl text-red-600 font-bold underline">
-    Hello world!
-    </h1>
+     
     </div>
   )
 }
