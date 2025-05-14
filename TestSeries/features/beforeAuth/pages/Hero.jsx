bef6c3f0b40c2c28f45765f62d9052a1daefeff9
Jay Fanse <jayf29112003@gmail.com>
2025-05-14 17:54:36 +0530

    plane adjusted

--- TestSeries/features/beforeAuth/pages/Hero.jsx
+++ TestSeries/features/beforeAuth/pages/Hero.jsx
@@ -30,10 +30,10 @@ const Hero = () => {
   return (
     <div className='mt-34 relative mx-50'>
       <img 
-      className='absolute left-0 top-0'
+      className='absolute left-0 top-0 w-2/12'
       src={planeLeft} alt='plane_left' />
       <img 
-      className='absolute right-0 top-1/2'
+      className='absolute right-0 top-5/12 w-1/12'
       src={planeRight} alt='plane_right' />
       <h1 className='
       pt-10
@@ -61,10 +61,7 @@ const Hero = () => {
       </div>
       <div className='w-full flex'>
         <button
-        className='
-        hover:cursor-pointer
-        bg-indigo-800 rounded-full text-white py-4 px-10 mt-20 mx-auto'
-        >
+        className='relative z-20 hover:cursor-pointer bg-indigo-800 rounded-full text-white py-4 px-10 mt-20 mx-auto'>
           <div className='flex'>
             <span className='pr-5 font-semibold text-xl '>
             Get Started For Free
