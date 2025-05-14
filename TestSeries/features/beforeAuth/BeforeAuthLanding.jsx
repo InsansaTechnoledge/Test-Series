2f3c7be6672ee9084ed9cd94d2c54de24f6153cc
Tanmay seth <tanmayseth@Tanmays-MacBook-Air.local>
2025-05-14 15:42:05 +0530

    Demo section code

--- TestSeries/features/beforeAuth/BeforeAuthLanding.jsx
+++ TestSeries/features/beforeAuth/BeforeAuthLanding.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import Hero from './pages/Hero'
+import WhyYourInstitute from './pages/WhyYourInstitute'
 
 const BeforeAuthLanding = () => {
     return (
+        <>
         <Hero />
+        <WhyYourInstitute/>
+        </>
     )
 }
 
---
+++ TestSeries/features/beforeAuth/pages/WhyYourInstitute.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import line from '../../../assests/Landing/Demo/line.svg';
+import thinking from '../../../assests/Landing/Demo/thinking.svg';
+
+const WhyYourInstitute = () => {
+  return (
+    <div className="relative w-full h-screen overflow-hidden">
+
+      {/* Full-screen Line Image */}
+      <img src={line} alt="line img" className='absolute left-1/2 top-0 -translate-x-1/2 z-10 h-full' />
+
+      <div className="relative z-10  flex flex-row items-center justify-between w-full h-full px-12">
+
+        {/* Left side */}
+        <div className="w-1/2 flex justify-center">
+          <img src={thinking} alt="thinking man image" className="max-h-screen" />
+        </div>
+
+        {/* Right side */}
+        <div className="w-1/2 px-8">
+          <h1 className="text-violet-950 font-bold text-4xl leading-snug">
+            Why your institute should register?
+          </h1>
+          <p className="mt-4 text-violet-900 text-left max-w-md">
+            Partnering with us provides your institution with cutting-edge tools designed to enhance
+            educational outcomes and streamline administrative processes.
+          </p>
+          <button className="bg-violet-800/10 text-xl text-violet-950 border-violet-950 border-2 rounded-lg py-2 px-8 mt-6">
+            Schedule demo
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default WhyYourInstitute;
