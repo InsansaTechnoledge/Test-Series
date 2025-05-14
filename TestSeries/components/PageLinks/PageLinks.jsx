3c7a9f7cca877a275413e824881f7d89555279c1
Jay Fanse <jayf29112003@gmail.com>
2025-05-14 13:59:20 +0530

    pagelinks created

---
+++ TestSeries/components/PageLinks/PageLinks.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+
+const PageLinks = () => {
+  return (
+    <Router>
+        <Routes>
+            
+        </Routes>
+    </Router>
+  )
+}
+
+export default PageLinks
