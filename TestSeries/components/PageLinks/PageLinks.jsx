5278d4c07d707ef3dc493a5c359bd41d5e1db0d2
Tanmay seth <tanmayseth@Tanmays-MacBook-Air.local>
2025-05-15 14:51:36 +0530

    authlayout created

--- TestSeries/components/PageLinks/PageLinks.jsx
+++ TestSeries/components/PageLinks/PageLinks.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import BeforeAuthLanding from '../../features/beforeAuth/BeforeAuthLanding';
 import BeforeAuthLayout from '../../layouts/BeforeAuthLayout';
+import AuthLayout from '../../layouts/authLayout';
+import InstituteLoginPage from '../../features/auth/pages/InstituteLoginPage';
 
 const PageLinks = () => {
   return (
@@ -10,7 +12,9 @@ const PageLinks = () => {
         <Route element={<BeforeAuthLayout />}>
           <Route path="/" element={<BeforeAuthLanding />} />
         </Route>
-      </Routes>
+        <Route element={<AuthLayout/>}/>
+          <Route path='/institute-registration' element={<InstituteLoginPage/>}/>
+        </Routes>
     </Router>
   );
 };
