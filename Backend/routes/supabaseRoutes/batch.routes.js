fc52fea28b27693946fdba30b756849aa7476063
BHADRIKA <bhadrika2226@gmail.com>
2025-05-15 16:01:58 +0530

    fix: enhance authentication middleware and update batch routes to include user role checks

--- Backend/routes/supabaseRoutes/batch.routes.js
+++ Backend/routes/supabaseRoutes/batch.routes.js
@@ -1,11 +1,11 @@
 import express from 'express'
-import { createOrgBatch, deleteOrgBatch, getOrgBatches, updateOrgBatch } from '../../controllers/SupabaseDB/batch.controllers.js'
-import { isOrganizationAuthenticated } from '../../middleware/isLoggedIn.middleware.js';
+import { createOrgBatch, deleteOrgBatch, getOrgBatches, updateOrgBatch} from '../../controllers/SupabaseDB/batch.controllers.js'
+import { isOrganizationAuthenticated,isOrgAndUserAuthenticated } from '../../middleware/isLoggedIn.middleware.js';
 
 const router = express.Router()
 
-router.post('/create-batch' , createOrgBatch)
-router.get('/get-batch', getOrgBatches);
+router.post('/create-batch' , isOrganizationAuthenticated,createOrgBatch)
+router.get('/get-batch',isOrgAndUserAuthenticated ,getOrgBatches);
 router.patch('/update-batch/:id' , updateOrgBatch)
 router.delete('delete-batch/:id' , deleteOrgBatch)
 
