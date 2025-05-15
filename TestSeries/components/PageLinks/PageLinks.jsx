06fb232a059302effa3f90e39e27fa6d569e06a6
Jay Fanse <jayf29112003@gmail.com>
2025-05-15 14:51:52 +0530

    login

--- TestSeries/components/PageLinks/PageLinks.jsx
+++ TestSeries/components/PageLinks/PageLinks.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import BeforeAuthLanding from '../../features/beforeAuth/BeforeAuthLanding';
 import BeforeAuthLayout from '../../layouts/BeforeAuthLayout';
+import LoginForm from '../../features/auth/pages/loginForm';
 
 const PageLinks = () => {
   return (
@@ -9,7 +10,9 @@ const PageLinks = () => {
       <Routes>
         <Route element={<BeforeAuthLayout />}>
           <Route path="/" element={<BeforeAuthLanding />} />
+          <Route path='/login' element={<LoginForm />} />
         </Route>
+
       </Routes>
     </Router>
   );
---
+++ TestSeries/features/auth/pages/loginForm.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const LoginForm = () => {
+  return (
+    <div>
+        <div className='grid grid-cols-12 mx-20 pt-20 min-h-screen'>
+            <div className='col-span-5 p-10'>
+                <h1 className='text-indigo-950 font-bold text-5xl'>
+                    Organization Login
+                </h1>
+                <div className=''>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi dolorem earum officia excepturi et omnis corrupti sapiente. Voluptatum, quaerat animi.
+                </div>
+                <button>
+
+                </button>
+            </div>
+            <div>
+                <form className='shadow-sm p-5'>
+
+                </form>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default LoginForm;
