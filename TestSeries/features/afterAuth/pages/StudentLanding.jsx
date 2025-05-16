1108140614aa1d213fd30b257ff234d0b212ab45
Tanmay seth <tanmayseth@Tanmays-MacBook-Air.local>
2025-05-16 11:51:52 +0530

    examlink component of studentlanding

--- TestSeries/features/afterAuth/pages/StudentLanding.jsx
+++ TestSeries/features/afterAuth/pages/StudentLanding.jsx
@@ -3,6 +3,7 @@ import StudentHero from '../components/StudentSide/StudentHero'
 import StudentDetails from '../components/StudentSide/StudentDetails'
 import AiWorkingSteps from '../components/StudentSide/AiWorkingSteps'
 import FacultySection from '../components/StudentSide/FacultySection'
+import ExamLinksComponent from '../components/StudentSide/ExamLinksComponent'
 
 const StudentLanding = () => {
     return (
@@ -13,9 +14,14 @@ const StudentLanding = () => {
                 <StudentDetails />
             </div>
         </div>
-                <AiWorkingSteps />
-                <div className="w-16 h-1 bg-blue-800 mx-auto mb-4 rounded"></div>
+            <ExamLinksComponent/>
+
+            <div className="w-16 h-1 bg-blue-800 mx-auto mb-4 rounded"></div>
+
+                {/* <AiWorkingSteps /> */}
+                {/* <div className="w-16 h-1 bg-blue-800 mx-auto mb-4 rounded"></div> */}
                 <FacultySection />
+
         </>
     )
 }
