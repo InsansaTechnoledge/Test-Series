d5b98f10aa607f4abbc7beb0db34280f3996f03d
Jay Fanse <jayf29112003@gmail.com>
2025-05-16 13:31:12 +0530

    refactor: margin button

--- TestSeries/features/afterAuth/components/StudentSide/StatsComponent/ViewAllButton.jsx
+++ TestSeries/features/afterAuth/components/StudentSide/StatsComponent/ViewAllButton.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 const ViewAllButton = () => {
   return (
-    <div className="flex justify-center items-center py-12">
+    <div className="flex justify-center items-center pt-12">
       <button className=" group hover:cursor-pointer bg-blue-800 text-gray-100 py-2 px-4 rounded-2xl flex items-center gap-2 hover:bg-blue-900 transition">
         View Detailed Dashboard
         <MoveRightIcon className="group-hover:translate-x-1 w-5 h-5 duration-300" />
