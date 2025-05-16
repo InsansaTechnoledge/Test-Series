547a3165ca1813a7df796b501c7505f2e9566ca1
Jay Fanse <jayf29112003@gmail.com>
2025-05-16 12:46:21 +0530

    stat dashboard

--- TestSeries/features/afterAuth/components/StudentSide/SubjectCards.jsx
+++ TestSeries/features/afterAuth/components/StudentSide/SubjectCards.jsx
@@ -6,7 +6,7 @@ const subjects = ["Physics", "Chemistry", "Maths", "English", "Computer Science"
 const SubjectCards = () => {
   return (
     <div className='bg-white py-16 px-6 md:px-20"'>
-        <h1 className='text-4xl text-center text-indigo-900 font-bold'>
+        <h1 className='text-4xl text-center text-blue-900 font-bold'>
             Subject Details
         </h1>
         <div className='grid md:grid-cols-2 lg:grid-cols-3 gap-8 px-40 mt-10'>
@@ -17,8 +17,8 @@ const SubjectCards = () => {
                     hover:cursor-pointer
                     hover:scale-105 transition-all duration-200 
                     relative overflow-hidden
-                    bg-gradient-to-br from-blue-200 to-white
-                    shadow-sm shadow-indigo-900 rounded-md text-indigo-900 font-bold text-2xl pt-8 pb-14 px-12'
+                    bg-blue-200/50
+                    shadow-sm shadow-blue-500/50 rounded-md text-blue-600 font-semibold text-2xl pt-8 pb-14 px-12'
                     key={idx}>
                         <span>
                             {subject}
@@ -27,9 +27,9 @@ const SubjectCards = () => {
                             {
                                 idx%2==0
                                 ?
-                                <Atom className='w-26 h-26 opacity-40'/>
+                                <Atom className='w-26 h-26 opacity-15'/>
                                 :
-                                <Codepen className='w-26 h-26 opacity-40'/>
+                                <Codepen className='w-26 h-26 opacity-15'/>
                             }
                         </div>
                     </div>
--- TestSeries/features/afterAuth/pages/StudentLanding.jsx
+++ TestSeries/features/afterAuth/pages/StudentLanding.jsx
@@ -5,6 +5,7 @@ import AiWorkingSteps from '../components/StudentSide/AiWorkingSteps'
 import FacultySection from '../components/StudentSide/FacultySection'
 import SubjectCards from '../components/StudentSide/SubjectCards'
 import ExamLinksComponent from '../components/StudentSide/ExamLinksComponent'
+import ExamStatsDashboard from '../components/StudentSide/ExamStatsDashboard'
 
 const StudentLanding = () => {
     return (
@@ -20,6 +21,7 @@ const StudentLanding = () => {
                 {/* <div className="w-16 h-1 bg-blue-800 mx-auto mb-4 rounded"></div> */}
                 <FacultySection />
                 <SubjectCards />
+                <ExamStatsDashboard />
         </>
     )
 }
