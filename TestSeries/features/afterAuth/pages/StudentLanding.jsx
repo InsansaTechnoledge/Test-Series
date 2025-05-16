72c96412fde799a25c518aec206e0be0eb0a27f9
Jay Fanse <jayf29112003@gmail.com>
2025-05-16 11:53:04 +0530

    subject cards

--- TestSeries/features/afterAuth/pages/StudentLanding.jsx
+++ TestSeries/features/afterAuth/pages/StudentLanding.jsx
@@ -3,6 +3,7 @@ import StudentHero from '../components/StudentSide/StudentHero'
 import StudentDetails from '../components/StudentSide/StudentDetails'
 import AiWorkingSteps from '../components/StudentSide/AiWorkingSteps'
 import FacultySection from '../components/StudentSide/FacultySection'
+import SubjectCards from '../components/StudentSide/SubjectCards'
 
 const StudentLanding = () => {
     return (
@@ -13,9 +14,10 @@ const StudentLanding = () => {
                 <StudentDetails />
             </div>
         </div>
-                <AiWorkingSteps />
+                {/* <AiWorkingSteps />
                 <div className="w-16 h-1 bg-blue-800 mx-auto mb-4 rounded"></div>
-                <FacultySection />
+                <FacultySection /> */}
+                <SubjectCards />
         </>
     )
 }
