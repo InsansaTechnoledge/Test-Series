6057598fd723d0527a2f44ebf141150ba92801c2
BHADRIKA <bhadrika2226@gmail.com>
2025-06-12 15:39:51 +0530

    Refactor contest creation logic to utilize RPC for batch insertion; streamline payload handling in createContestQuery

--- Backend/utils/SqlQueries/contest.queries.js
+++ Backend/utils/SqlQueries/contest.queries.js
@@ -2,12 +2,27 @@ import { getSupabaseClient } from "../../database/SupabaseDB.js";
 
 const supabase = getSupabaseClient();
 
-export const createContestQuery=async(contestData)=>{
+export const createContestQuery = async (payload) => {
+    const rpcPayload = {
+        p_selected_batches: payload.selectedBatches, 
+        p_organization_id: payload.organization_id,
+        p_name: payload.name,
+        p_type: payload.type,
+        p_schedule: payload.schedule,
+        p_duration: payload.duration,
+        p_live_until: payload.live_until,
+        p_go_live: payload.go_live,
+        p_created_at: payload.created_at,
+        p_updated_at: payload.updated_at,
+        p_description: payload.description,
+        p_validity: payload.validity 
+    };
+
+
+
     const { data, error } = await supabase
-        .from("organization_contest")
-        .insert(contestData)
-        .select()
-        .single();
+        .rpc('create_contest_with_batches', rpcPayload)
+        .select();
 
     if (error) throw error;
     return data;
