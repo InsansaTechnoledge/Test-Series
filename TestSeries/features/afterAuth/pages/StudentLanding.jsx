a332ee437d669d7020447964ba774f11439ffc7d
kashishgadhiya <kashishgadhiya24@gmail.com>
2025-07-02 16:56:16 +0530

    Refactor :student ui

--- TestSeries/features/afterAuth/pages/StudentLanding.jsx
+++ TestSeries/features/afterAuth/pages/StudentLanding.jsx
@@ -6,25 +6,48 @@ import ExamLinksComponent from '../components/StudentSide/Landing/ExamLinksCompo
 import { cards } from '../data/DisplayComponentData'
 import ExamStatsDashboard from '../components/StudentSide/Landing/ExamStatsDashboard'
 import HeadingUtil from '../utility/HeadingUtil';
-
+import BatchInfoCard from "../components/StudentSide/Landing/BatchInfoCard"
+import ExamComponent from '../components/StudentSide/Landing/ExamComponent'
 const StudentLanding = () => {
     return (
         <>
-        <div className='m-2 md:m-10 rounded-4xl overflow-hidden shadow-md'>
-            <StudentHero />
-            <div className=' py-8'>
-                <StudentDetails />
+
+
+
+
+
+
+            <div>
+                {/* /student and profiles */}
+                <div className='m-4 md:m-10 rounded-4xl overflow-hidden shadow-md flex flex-col lg:flex-row'>
+
+                    <div className='w-full lg:w-1/3 flex-shrink-0'>
+                        <StudentDetails />
+                    </div>
+
+
+                    <div className='w-full lg:w-2/3 flex-grow'>
+                        <StudentHero />
+                    </div>
+                </div>
+                <div className='m-4 md:m-10 rounded-4xl overflow-hidden shadow-md flex flex-col lg:flex-row'>
+
+<BatchInfoCard/>
+<ExamComponent/>
+                </div>
+
             </div>
-        </div>  
-        <div className='my-12'>
-                <HeadingUtil heading="Quick Link's to Exams" description="View and access all your examination resources"/>
+
+
+            <div className='my-12'>
+                <HeadingUtil heading="Quick Link's to Exams" description="View and access all your examination resources" />
                 <ExamLinksComponent Data={cards} />
-        </div>
-                {/* <AiWorkingSteps /> */}
-                {/* <div className="w-16 h-1 bg-blue-800 mx-auto mb-4 rounded"></div> */}
-                <FacultySection />
-                {/* <SubjectCards /> */}
-                <ExamStatsDashboard/>
+            </div>
+            {/* <AiWorkingSteps /> */}
+            {/* <div className="w-16 h-1 bg-blue-800 mx-auto mb-4 rounded"></div> */}
+            <FacultySection />
+            {/* <SubjectCards /> */}
+            <ExamStatsDashboard />
         </>
     )
 }
